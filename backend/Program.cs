478899b278d36339fb8dc1fc39c876d77f83d4fb
NKVDream <kmv235@yandex.ru>
2026-05-21 08:23:50 +0500

    program updated

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,56 +1,3 @@
-/*
-using Microsoft.EntityFrameworkCore;
-using Egov.Data;
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policy =>
-    {
-        policy.WithOrigins("http://localhost:3000") // Порт, на котором работает React
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
-});
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(connectionString));
-
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        // Дополнительная защита: предотвращает бесконечные циклы при сериализации,
-        // если где-то в коде случайно вернутся "сырые" модели вместо DTO
-        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
-    });
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Egov Wiki API v1");
-        c.RoutePrefix = string.Empty;
-    });
-}
-
-app.UseCors();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-*/
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -101,7 +48,7 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Egov Wiki API", Version = "v1" });
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Egov API", Version = "v1" });
 
     options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme//схема безопасности JWT Bearer для свагера
     {
