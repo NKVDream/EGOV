62852498b0cd6bcddd9f17039cf63852e0f6406c
NKVDream <kmv235@yandex.ru>
2026-05-22 08:39:38 +0500

    Front - react v0.1

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80c127c89888e76c53c3010a25d9e2c11b01cb91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dbe416bf4e6e18b78aee5ff11fecfdaac0ccb4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
