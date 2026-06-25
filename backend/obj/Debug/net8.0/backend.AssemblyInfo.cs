90d7ff2fd2d38e979473bbf16f8c2e337c4dc3d7
NKVDream <kmv235@yandex.ru>
2026-06-25 09:00:45 +0500

    Netanyahu's Final Order

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6889acdefc77e59942acc71a120f701971c9410c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78b143c8e0e9c5f30cad959e2b03944fd11fba27")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
